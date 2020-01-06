7993ca922e94f532d6c7c108793b0d3c609857ce
Christopher Lozinski <lozinski@PythonLinks.info>
2020-01-06 09:09:16 +0000

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # python-mqtt-class-notes
 These are the instructions for a Python MQTT class.
+There are two parts to the class.  First we build a client
+application.  Really two applications, one to subscribe and one to publish.
+Then we will fire up a server.
+
+
+
+
+## Client Exercises
+
+1. Connect to the MQTT server.
+
+2. On Connect, go ahead and subscribe.  You can do it right after the connect,
+but more interesting to do it on subscribe.
+
+3. OnSubscribed, go ahead and publish a message.  Again you can probably do it
+right after the connect/subscribe command, but more interesting to do it onSubscribed
+You learn how the call backs work.
+
+4. Okay, and now you can create a similar application for waiting at the terminal and
+publishing what you type.  But give it a very very long connection time out. 
+
+## Broker Exercise
+I will first lead a discussion of the different brokers.
+Basically Mosquitto with the go auth plugin seems to be the richest and most stable.
+Hive in Java is interesting. Polished and expensive. 
+EMQX in Erlang has lots of functionality.
+Emitter.io in GoLang is interesting, but odd. 
+
+1. Now go ahead and fire up a broker.  
+https://hub.docker.com/r/pythonlinks/letsencrypt-mosquitto
+
+2. Make sure it works using MQTT Explorer.
+
+3. Connect your client and server to it. 
---
+++ client.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# This example shows how you can use the MQTT client in a class.
+
+import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.client as mqtt
+
+class MyMQTTClass(mqtt.Client):
+
+    def on_connect(self, mqttc, obj, flags, rc):
+        pass
+
+    def on_message(self, mqttc, obj, msg):
+        pass
+
+    def on_publish(self, mqttc, obj, mid):
+        pass
+
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
+        pass
+
+    def on_log(self, mqttc, obj, level, string):
+        pass
+
+    def run(self):
+        # connect to mqtt.eclipse.org, port 80 
+        #self.connect()
+        rc = 0
+        while rc == 0:
+            rc = self.loop()
+        return rc
+
+
+# If you want to use a specific client id, use
+# mqttc = MyMQTTClass("client-id")
+# but note that the client id must be unique on the broker. Leaving the client
+# id parameter empty will generate a random id for you.
+mqttc = MyMQTTClass()
+rc = mqttc.run()
+
+
+
+# Copyright (c) 2013 Roger Light <roger@atchoo.org>
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Distribution License v1.0
+# which accompanies this distribution.
+#
+# The Eclipse Distribution License is available at
+#   http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# Contributors:
+#    Roger Light - initial implementation
